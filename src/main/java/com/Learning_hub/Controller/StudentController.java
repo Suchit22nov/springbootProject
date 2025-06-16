b0680ca2d62ac828ab826e6232a52b7e5cce6183
Suchit Verma <suchitnov00@gmail.com>
2025-06-16 11:37:12 +0530

    error fix

--- src/main/java/com/Learning_hub/Controller/StudentController.java
+++ src/main/java/com/Learning_hub/Controller/StudentController.java
@@ -15,7 +15,7 @@ public class StudentController {
 
     @Autowired
     public StudentService studentServices;
-    //Used for local Storage when database is not present
+
     private final Map<Long, StudentEntity> studentsDetail = new HashMap<>();
 
     @GetMapping
@@ -29,7 +29,6 @@ public class StudentController {
     }
 
 
-
     @GetMapping("id/{studentId}")
     public ResponseEntity<StudentEntity> getStudentById(@PathVariable Long studentId ) {
         StudentEntity getwork= studentServices.getStudentById(studentId);
@@ -38,7 +37,7 @@ public class StudentController {
         }
         return  new ResponseEntity<>( HttpStatus.BAD_REQUEST);
 
-
+    }
 
     @PostMapping
     public ResponseEntity<StudentEntity> postData(@RequestBody StudentEntity studentEntries) {
